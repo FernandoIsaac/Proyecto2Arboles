2b7b537223e83736cc03b60cb9e9908cfc245741
Mauricio Matamoros <mauricio.enrique.matamoros@gmail.com>
2015-11-18 20:13:50 -0600

    Consola terminada

--- src/classes/Console.java
+++ src/classes/Console.java
@@ -12,11 +12,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
@@ -27,8 +29,15 @@ import javax.swing.text.StyledDocument;
  */
 public class Console {
 
+    //System.getProperty("user.dir") //actual string
+    
+    File actual;
+    int bandera = 0;
+    File root = FileSystemView.getFileSystemView().getHomeDirectory();
+
     public static void main(String[] args) {
         new Console();
+        
     }
     public JFrame frame;
     public JTextPane console;
@@ -43,6 +52,10 @@ public class Console {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
         }
+        if (bandera == 0) {
+            actual = root;
+            bandera++;
+        }
         frame = new JFrame();
         frame.setTitle("Console");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,7 +68,7 @@ public class Console {
         document = console.getStyledDocument();
 
         input = new JTextField();
-        // input.setBorder(null)
+        //input.setBorder(null);
         input.setEditable(true);
         input.setFont(new Font("Courier New", Font.PLAIN, 12));
         input.setForeground(Color.BLACK);
@@ -69,7 +82,7 @@ public class Console {
                 String text = input.getText();
 
                 if (text.length() > 1) {
-                   // print(text + "\n", true);
+                    println(text, true);
                     doCommand(text);
                     scrollBottom();
                     input.selectAll();
@@ -109,53 +122,124 @@ public class Console {
 
         frame.setResizable(false);
         frame.setVisible(true);
+        
+        println("",true);
 
     }
 
     public void doCommand(String s) {
+        final String[] commands = s.split(" ");
 
-        
-        
-        
+        try {
+            if (commands[0].startsWith("ls")) {
+                list(actual.listFiles());
+            }else if (commands[0].startsWith("cd")) {
+                String message = "";
+                for (int i = 0; i < commands.length; i++) {
+                    message += commands[i];
+                    if (i != commands.length - 1) {
+                        message += " ";
+                    }
+                }
+                changeDirectory(message);
+            }else if (commands[0].equals("clear")) {
+                clear();
+            }
+
+        } catch (Exception e) {
+        }
+
     }
-    
-    public void scrollTop(){
+
+    public void scrollTop() {
         console.setCaretPosition(0);
     }
-    
-    public void scrollBottom(){
+
+    public void scrollBottom() {
         console.setCaretPosition(console.getDocument().getLength());
     }
-    
-    public void print(String s, boolean trace){
-        print(s,trace,new Color (255, 255, 255));
+
+    public void print(String s, boolean trace) {
+        print(s, trace, new Color(255, 255, 255));
     }
-    
-    public void print(String s, boolean trace, Color c){
+
+    public void print(String s, boolean trace, Color c) {
         Style style = console.addStyle("Style", null);
         StyleConstants.setForeground(style, c);
-        
+
         if (trace) {
             Throwable t = new Throwable();
             // puedo usar esto para display el path
-            StackTraceElement[] elements = t.getStackTrace();
-            String caller = elements[0].getClassName();
-            
-            s= caller + " -> " + s; 
+            //StackTraceElement[] elements = t.getStackTrace();
+            //String caller = elements[0].getClassName();
+
+            s = actual.getPath() + " -> " + s;
         }
-        
+
         try {
             document.insertString(document.getLength(), s, style);
         } catch (Exception e) {
         }
     }
-    
-    public void println(String s, boolean trace){
-        println(s,trace, new Color(255,255,255));
+
+    public void println(String s, boolean trace) {
+        println(s, trace, new Color(255, 255, 255));
+
     }
-    
-    public void println(String s, boolean trace, Color c){
-    
+
+    public void println(String s, boolean trace, Color c) {
+        print(s + "\n", trace, c);
     }
-    
+
+    public void changeDirectory(String s) {
+        if (s.contains("cd ../")) {
+            try {
+                actual = root;
+            } catch (Exception e) {
+            }
+            
+        }else if (s.contains("cd ./")) {
+            try {
+                actual = new File(actual.getParent());
+            } catch (Exception e) {
+            }   
+        }else{
+            try {
+                String message = "";
+                for (int i = 0; i < s.length(); i++) {
+                    if (i > 2) {
+                        message += s.charAt(i);
+                    }
+                }
+                actual = new File(actual.getPath() + "/" + message);
+            } catch (Exception e) {
+            }
+                    
+            
+        }
+    }
+
+    public void list(File[] files) {
+        for (File file : files) {
+            if (file.isDirectory() && file.getName().startsWith(".")) {
+
+            } else if (file.isDirectory()) {
+                println(file.getName(),false);
+            } else if (file.getName().startsWith(".")) {
+
+            } else {
+                println(file.getName(),false);
+            }
+        }
+    }
+
+    public void clear() {
+        try {
+            document.remove(0, document.getLength());
+
+        } catch (Exception e) {
+        }
+
+    }
+
 }
